552724f5a1fdce6380918e6f9552c77590827b1b
PepsaCola <162103453+PepsaCola@users.noreply.github.com>
2024-12-04 10:07:29 +0100

    com

---
+++ dist/cosmetics.52d815dc.js
@@ -0,0 +1,6 @@
+!async function(){let e=document.querySelector(".cosmetic-search-result");e.innerHTML="";try{let t=await fetch("http://localhost:3000/api/search-results");for(let r of(await t.json()))e.insertAdjacentHTML("beforeend",`
+    <div class="result-item">
+      <img src="${r.brI||r.insI||r.trI||r.legI||r.carI}" alt="${r.itemID}">
+    </div>
+  `)}catch(e){console.error("Error fetching search results:",e)}}();
+//# sourceMappingURL=cosmetics.52d815dc.js.map
